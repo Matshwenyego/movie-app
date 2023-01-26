4dd8f5b07723a84077ab0044cd1741dfeb7c9664
Matshwenyego Ngoetjana <Ashley.Ngoetjana@multichoice.co.za>
2023-01-26 11:10:27 +0200

    NavBar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { CssBaseline } from "@mui/material";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div>HELLO WORLD</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
