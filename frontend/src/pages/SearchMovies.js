e6d937702cb065a5ae9d723e045d19928a9bdee9
Matshwenyego Ashley Ngoetjana <Ashleyngoetjana@gmail.com>
2025-01-05 11:20:39 +0200

    fix: endpoint

--- frontend/src/pages/SearchMovies.js
+++ frontend/src/pages/SearchMovies.js
@@ -10,8 +10,7 @@ export default function SearchMovies(props) {
   const handleSearchMovies = async (queries) => {
     const { s } = queries;
     const response = get(
-      // `${process.env.REACT_APP_BASE_URL}/?s=${s}&apikey=${process.env.REACT_APP_API_KEY}`
-      `/movies?s=${s}`
+      `${process.env.REACT_APP_BASE_URL}/?s=${s}&apikey=${process.env.REACT_APP_API_KEY}`
     );
     const { Search } = await response;
     setMovies(Search);
